065b946627d351e167cf3ab05679ec020b42aefc
gizmore <gizmore@wechall.net>
2025-11-18 13:48:47 +0100

    'sd'

--- method/mail.py
+++ method/mail.py
@@ -62,7 +62,7 @@ class mail(MethodCronjob):
             user.render_name(),
             sitename(),
             poll.render_title(),
-            poll.render_descr(Mode.MAIL),
+            poll.render_descr(Mode.mail),
             "<br/>\n".join(links),
         )
         mail.subject(t('mails_poll_created', (poll.render_title(),)))
